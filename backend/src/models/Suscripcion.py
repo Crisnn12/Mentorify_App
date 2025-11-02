e6d7fbbf785b79d3c9acd49f52ad6636d2756811
emiqia <165110014+emiqia@users.noreply.github.com>
2025-11-02 13:00:32 -0300

    Suscripcion.py

--- backend/src/models/Suscripcion.py
+++ backend/src/models/Suscripcion.py
@@ -1,26 +1,22 @@
 from datetime import date
 
 class Suscripcion:
-    def __init__(self, id_suscripcion: int, costo: float, fecha_inicio: date, fecha_expiracion: date): #Complejidad de 1
+    def __init__(self, id_suscripcion: int, costo: float, fecha_inicio: date, fecha_expiracion: date):
         self.id_suscripcion = id_suscripcion
         self.costo = costo
         self.fecha_inicio = fecha_inicio
         self.fecha_expiracion = fecha_expiracion
         self.activa = False
-        #+1 por base de la función
 
-    def pagar(self) -> None: #Complejidad de 1
+    def pagar(self) -> None:
         print(f"Pago de suscripción de ${self.costo} procesado.")
-        #+1 por base de la función
 
-    def activar_suscripcion(self) -> None: #Complejidad de 2
-        if self.fecha_expiracion >= date.today(): # +1
+    def activar_suscripcion(self) -> None: 
+        if self.fecha_expiracion >= date.today(): 
             self.activa = True
             print("Suscripción activada con éxito.")
-        else: #+1
+        else:
             print("Error: La fecha de expiración ya pasó.")
-        #+1 por base de la función
 
-    def __repr__(self) -> str: #Complejidad de 1
+    def __repr__(self) -> str: 
         return f"Suscripcion(ID='{self.id_suscripcion}', Activa='{self.activa}', Costo='${self.costo}')"
-        #+1 por base de la función
