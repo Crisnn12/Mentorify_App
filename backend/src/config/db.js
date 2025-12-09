9b34efc237343582ca04c373a913cc5d306c40de
emiqia <165110014+emiqia@users.noreply.github.com>
2025-12-09 18:42:01 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.MONGO_URI; 
+const DB_URL = "mongodb+srv://emixia:sunnychu22@clustermentorify.ruyzg4a.mongodb.net/MentorifyDB?retryWrites=true&w=majority";
+
 const connectDB = async () => {
     try {
         if (!DB_URL) {
-            throw new Error("MONGO_URI no está definida en el archivo .env.");
+            throw new Error("DB_URL no está definida.");
         }
         await mongoose.connect(DB_URL);
         console.log('Conexión a MongoDB establecida con éxito.');
     } catch (error) {
-        console.error('Detalles del error:', error.message);
+        console.error('ERROR CRÍTICO DE CONEXIÓN A MONGODB:', error.message);
         process.exit(1); 
     }
 };
+
 module.exports = connectDB;
